6cbd18afd38c54776efe65b2f9abdc2f4eac6963
Mohammed <MD.2015.1@outlook.com>
2020-09-21 11:10:14 +0330

    Create Example.ino

---
+++ Example/Example.ino
@@ -0,0 +1,17 @@
+#include <Q2HX711.h>
+#include <Average.h>
+
+const byte MPS_OUT_pin = 2; // OUT data pin
+const byte MPS_SCK_pin = 3; // clock data pin
+int avg_size = 10; // #pts to average over
+
+Q2HX711 MPS20N0040D(MPS_OUT_pin, MPS_SCK_pin); // start comm with the HX710B
+Average<long> ave(10);
+void setup() {
+  Serial.begin(9600); // start the serial port
+}
+
+void loop() {
+ave.push(MPS20N0040D.read());
+Serial.println(ave.mean());
+}
